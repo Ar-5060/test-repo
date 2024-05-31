916fe425109ffa886c75d9d486bbe52711b2b9bd
Ar5060 <arrahman5060@gmail.com>
2024-05-31 16:17:58 +0600

    first changes

---
+++ test.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int digit=0;
+    int small=0,capital=0;
+    char str[40]="habib is Good boy";
+    for (int i = 0; str[i]!='\0'; i++)
+    {
+        
+
+        if (str[i]>=65 && str[i]<=90 )
+        {
+            capital++;
+        }
+        else if (str[i]>=97 && str[i]<=122) {
+            small++;
+        }
+       
+        digit++;
+    }
+            printf("Total number of vowel :%d\n",digit);
+            printf("Total number of capital :%d\n",capital);
+            printf("Total number of small :%d\n",small);
+    
+    return 0;
+
+}
